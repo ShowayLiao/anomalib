57dd970c0b5162ac0ee69d67cf7f41c7df4c0943
PC2 <u202110733@hust.edu.cn>
2026-05-21 19:12:26 +0800

    暂存70准确度

--- scripts/limr/test_aebad_s.bat
+++ scripts/limr/test_aebad_s.bat
@@ -3,7 +3,7 @@ cd /d %~dp0..\..
 call .venv\Scripts\activate.bat
 python tools\limr\test.py ^
     --dataset aebad_s ^
-    --root ./datasets/AeBAD/AeBAD_S ^
+    --root I:\exp\datasets\AeBAD\AeBAD_S ^
     --category AeBAD_S ^
     --image-size 256 ^
     --train-batch-size 16 ^
@@ -11,6 +11,6 @@ python tools\limr\test.py ^
     --backbone resnet34 ^
     --alpha 1.75 ^
     --aebad-s-domain-shift same ^
-    --checkpoint ./output_limr/aebad_s/LiMR/AeBAD_S/v1/weights/lightning/model.ckpt ^
+    --checkpoint .\output_limr\aebad_s\LiMR\AeBAD_S\AeBAD_S\v2\weights\lightning\model.ckpt ^
     --output-dir ./output_limr/aebad_s/test_same
 pause
